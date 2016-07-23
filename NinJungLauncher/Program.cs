f412f67239486199d9338a3d7625e961fe7eb651
steamb23 <steamb23@gmail.com>
2016-07-24 02:15:46 +0900

    NInJungLauncher 추가

---
+++ NinJungLauncher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+using NInJung;
+
+namespace NinJungLauncher
+{
+    class Options
+    {
+        [Option('u', "unstrict", DefaultValue = false, HelpText = "Interpreted as unstrict mode.")]
+        public bool Unstrict
+        {
+            get;
+            set;
+        }
+        [Option('p',"path", Required = true, HelpText = "File path.")]
+        public string Path
+        {
+            get;
+            set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var option = new Options();
+            var helpText = HelpText.AutoBuild(option);
+            helpText.Copyright = CopyrightInfo.Default + " SteamB23";
+            if (Parser.Default.ParseArguments(args, option))
+            {
+                new NInJung.ScriptEngine((option.Unstrict ? ScriptEngineMode.Unstrict : ScriptEngineMode.Strict)).ExecuteFromFile(option.Path);
+            }
+            else
+            {
+                Console.WriteLine(helpText);
+            }
+        }
+    }
+}
